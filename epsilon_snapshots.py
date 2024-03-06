995a576a3498444eb4aac776c220bb4a0c9e8825
Luis H. P. Mendes <luishpmendes@gmail.com>
2024-03-06 19:29:24 -0300

    Delete and add snapshot images, and update snapshot CSV files

--- epsilon_snapshots.py
+++ epsilon_snapshots.py
@@ -9,7 +9,7 @@ metrics_snapshots_filename = 'metrics_snapshots.csv'
 metrics_snapshots_df = pd.read_csv(metrics_snapshots_filename)
 
 # Set target values for each metric
-target_value = 0.70
+target_value = 0.91
 
 # Filter data for the Multiplicative Epsilon Indicator
 epsilon_snapshots_df = metrics_snapshots_df[metrics_snapshots_df['metric name'] == 'Multiplicative Epsilon Indicator']
@@ -82,7 +82,7 @@ for solver in solvers:
 # Convert the cumulative distribution to a DataFrame for easier plotting
 cumulative_distribution_df = pd.DataFrame(cumulative_distribution, index=time_values)
 
-cumulative_distribution_df.to_csv('epsilon_snapshots.csv')
+cumulative_distribution_df.to_csv('epsilon_snapshots_easy.csv')
 
 # Plot the performance profile
 plt.figure()
@@ -95,4 +95,99 @@ plt.xscale("log")
 plt.legend(loc='best')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 # Save the plot
-plt.savefig('epsilon_snapshots.png')
+plt.savefig('epsilon_snapshots_easy.png')
+
+
+
+# Load the data
+metrics_snapshots_filename = 'metrics_snapshots.csv'
+metrics_snapshots_df = pd.read_csv(metrics_snapshots_filename)
+
+# Set target values for each metric
+target_value = 0.95
+
+# Filter data for the Multiplicative Epsilon Indicator
+epsilon_snapshots_df = metrics_snapshots_df[metrics_snapshots_df['metric name'] == 'Multiplicative Epsilon Indicator']
+
+# Initialize a dictionary to hold the cumulative distribution for each solver
+cumulative_distribution = {solver: [] for solver in solvers}
+
+instances = epsilon_snapshots_df['instance'].unique()
+seeds = epsilon_snapshots_df['seed'].unique()
+
+time_values = []
+epsilon_snapshots_of_solver_instance_seed: dict[str, dict[str, dict[int, pd.DataFrame]]] = {}
+for solver in solvers:
+    epsilon_snapshots_of_solver_instance_seed[solver] = {}
+    for instance in instances:
+        epsilon_snapshots_of_solver_instance_seed[solver][instance] = {}
+        for seed in seeds:
+            epsilon_snapshots_of_solver_instance_seed[solver][instance][seed] = epsilon_snapshots_df[(epsilon_snapshots_df['solver'] == solver) & (epsilon_snapshots_df['instance'] == instance) & (epsilon_snapshots_df['seed'] == seed)]
+            time_values_temp = epsilon_snapshots_of_solver_instance_seed[solver][instance][seed]['snapshot time'].unique()
+            if len(time_values) == 0 or time_values[0] < time_values_temp[0]:
+                time_values = time_values_temp
+
+epsilon_snapshots_of_solver_instance_seed_time: dict[str, dict[str, dict[int, dict[float, pd.DataFrame]]]] = {}
+for solver in solvers:
+    epsilon_snapshots_of_solver_instance_seed_time[solver] = {}
+    for instance in instances:
+        epsilon_snapshots_of_solver_instance_seed_time[solver][instance] = {}
+        for seed in seeds:
+            epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed] = {}
+            for time in time_values:
+                epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed][time] = epsilon_snapshots_of_solver_instance_seed[solver][instance][seed][epsilon_snapshots_of_solver_instance_seed[solver][instance][seed]['snapshot time'] <= time]
+                greatest_time = epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed][time]['snapshot time'].max()
+                epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed][time] = epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed][time][epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed][time]['snapshot time'] == greatest_time]
+
+# Calculate the cumulative distribution for each solver
+for solver in solvers:
+    print(solver)
+    for time in time_values:
+        num_meeting_target = 0.0
+        num_not_meeting_target = 0.0
+        for instance in instances:
+            for seed in seeds:
+                if not epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed][time].empty > 0:
+                    if epsilon_snapshots_of_solver_instance_seed_time[solver][instance][seed][time]["metric value"].values[0] >= target_value:
+                        num_meeting_target += 1
+                    else:
+                        num_not_meeting_target += 1
+        if num_meeting_target + num_not_meeting_target == len(instances) * len(seeds):
+            cumulative_distribution[solver].append(100 * (num_meeting_target / (num_meeting_target + num_not_meeting_target)))
+        else:
+            cumulative_distribution[solver].append(-1.0)
+
+num_any_negatives = 0
+while True:
+    any_negatives = False
+    for solver in solvers:
+        if cumulative_distribution[solver][num_any_negatives] < 0:
+            any_negatives = True
+            break
+    if any_negatives:
+        num_any_negatives += 1
+    else:
+        break
+
+# Remove the first num_any_zeros elements from the cumulative distribution
+time_values = time_values[num_any_negatives:]
+for solver in solvers:
+    cumulative_distribution[solver] = cumulative_distribution[solver][num_any_negatives:]
+
+# Convert the cumulative distribution to a DataFrame for easier plotting
+cumulative_distribution_df = pd.DataFrame(cumulative_distribution, index=time_values)
+
+cumulative_distribution_df.to_csv('epsilon_snapshots_hard.csv')
+
+# Plot the performance profile
+plt.figure()
+for i in range(len(solvers)):
+    plt.plot(cumulative_distribution_df.index, cumulative_distribution_df[solvers[i]], label=solvers[i], marker = (i + 3, 2, 0), color = colors[i], alpha = 0.80)
+plt.title('Time-to-Target for Multiplicative Epsilon Indicator')
+plt.xlabel('Time')
+plt.ylabel('Percentage of Executions')
+plt.xscale("log")
+plt.legend(loc='best')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+# Save the plot
+plt.savefig('epsilon_snapshots_hard.png')
--- hvr_snapshots.py
+++ hvr_snapshots.py
@@ -9,7 +9,7 @@ metrics_snapshots_filename = 'metrics_snapshots.csv'
 metrics_snapshots_df = pd.read_csv(metrics_snapshots_filename)
 
 # Set target values for each metric
-target_value = 0.60
+target_value = 0.70
 
 # Filter data for the Hypervolume Ratio
 hvr_snapshots_df = metrics_snapshots_df[metrics_snapshots_df['metric name'] == 'Hypervolume Ratio']
@@ -82,7 +82,7 @@ for solver in solvers:
 # Convert the cumulative distribution to a DataFrame for easier plotting
 cumulative_distribution_df = pd.DataFrame(cumulative_distribution, index=time_values)
 
-cumulative_distribution_df.to_csv('hvr_snapshots.csv')
+cumulative_distribution_df.to_csv('hvr_snapshots_easy.csv')
 
 # Plot the performance profile
 plt.figure()
@@ -95,4 +95,99 @@ plt.xscale("log")
 plt.legend(loc='best')
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 # Save the plot
-plt.savefig('hvr_snapshots.png')
+plt.savefig('hvr_snapshots_easy.png')
+
+
+
+# Load the data
+metrics_snapshots_filename = 'metrics_snapshots.csv'
+metrics_snapshots_df = pd.read_csv(metrics_snapshots_filename)
+
+# Set target values for each metric
+target_value = 0.90
+
+# Filter data for the Hypervolume Ratio
+hvr_snapshots_df = metrics_snapshots_df[metrics_snapshots_df['metric name'] == 'Hypervolume Ratio']
+
+# Initialize a dictionary to hold the cumulative distribution for each solver
+cumulative_distribution = {solver: [] for solver in solvers}
+
+instances = hvr_snapshots_df['instance'].unique()
+seeds = hvr_snapshots_df['seed'].unique()
+
+time_values = []
+hvr_snapshots_of_solver_instance_seed: dict[str, dict[str, dict[int, pd.DataFrame]]] = {}
+for solver in solvers:
+    hvr_snapshots_of_solver_instance_seed[solver] = {}
+    for instance in instances:
+        hvr_snapshots_of_solver_instance_seed[solver][instance] = {}
+        for seed in seeds:
+            hvr_snapshots_of_solver_instance_seed[solver][instance][seed] = hvr_snapshots_df[(hvr_snapshots_df['solver'] == solver) & (hvr_snapshots_df['instance'] == instance) & (hvr_snapshots_df['seed'] == seed)]
+            time_values_temp = hvr_snapshots_of_solver_instance_seed[solver][instance][seed]['snapshot time'].unique()
+            if len(time_values) == 0 or time_values[0] < time_values_temp[0]:
+                time_values = time_values_temp
+
+hvr_snapshots_of_solver_instance_seed_time: dict[str, dict[str, dict[int, dict[float, pd.DataFrame]]]] = {}
+for solver in solvers:
+    hvr_snapshots_of_solver_instance_seed_time[solver] = {}
+    for instance in instances:
+        hvr_snapshots_of_solver_instance_seed_time[solver][instance] = {}
+        for seed in seeds:
+            hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed] = {}
+            for time in time_values:
+                hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed][time] = hvr_snapshots_of_solver_instance_seed[solver][instance][seed][hvr_snapshots_of_solver_instance_seed[solver][instance][seed]['snapshot time'] <= time]
+                greatest_time = hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed][time]['snapshot time'].max()
+                hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed][time] = hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed][time][hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed][time]['snapshot time'] == greatest_time]
+
+# Calculate the cumulative distribution for each solver
+for solver in solvers:
+    print(solver)
+    for time in time_values:
+        num_meeting_target = 0.0
+        num_not_meeting_target = 0.0
+        for instance in instances:
+            for seed in seeds:
+                if not hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed][time].empty > 0:
+                    if hvr_snapshots_of_solver_instance_seed_time[solver][instance][seed][time]["metric value"].values[0] >= target_value:
+                        num_meeting_target += 1
+                    else:
+                        num_not_meeting_target += 1
+        if num_meeting_target + num_not_meeting_target == len(instances) * len(seeds):
+            cumulative_distribution[solver].append(100 * (num_meeting_target / (num_meeting_target + num_not_meeting_target)))
+        else:
+            cumulative_distribution[solver].append(-1.0)
+
+num_any_negatives = 0
+while True:
+    any_negatives = False
+    for solver in solvers:
+        if cumulative_distribution[solver][num_any_negatives] < 0:
+            any_negatives = True
+            break
+    if any_negatives:
+        num_any_negatives += 1
+    else:
+        break
+
+# Remove the first num_any_zeros elements from the cumulative distribution
+time_values = time_values[num_any_negatives:]
+for solver in solvers:
+    cumulative_distribution[solver] = cumulative_distribution[solver][num_any_negatives:]
+
+# Convert the cumulative distribution to a DataFrame for easier plotting
+cumulative_distribution_df = pd.DataFrame(cumulative_distribution, index=time_values)
+
+cumulative_distribution_df.to_csv('hvr_snapshots_hard.csv')
+
+# Plot the performance profile
+plt.figure()
+for i in range(len(solvers)):
+    plt.plot(cumulative_distribution_df.index, cumulative_distribution_df[solvers[i]], label=solvers[i], marker = (i + 3, 2, 0), color = colors[i], alpha = 0.80)
+plt.title('Time-to-Target for Hypervolume Ratio')
+plt.xlabel('Time')
+plt.ylabel('Percentage of Executions')
+plt.xscale("log")
+plt.legend(loc='best')
+plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+# Save the plot
+plt.savefig('hvr_snapshots_hard.png')
